8cddfb13c3006f35f8fe71ef4e635a0090d983e3
surajit77707 <surajitsingh77707@gmail.com>
2025-01-27 01:13:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7556948125:AAGto99b2MUfHksyBdrfMdxYgNaGAk5GnDc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "23967991"))
@@ -14,8 +14,8 @@ ADMINS = int(os.environ.get("ADMINS", "7638575366"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://surajit4207:surajit4207@restricted.oyc1d.mongodb.net/?retryWrites=true&w=majority&appName=restricted") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "restricted")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
