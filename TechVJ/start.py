b826b162c4d71243c3b4cc978241ec2c1bd1f0c4
surajit77707 <surajitsingh77707@gmail.com>
2025-01-27 00:46:58 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 import os
 import asyncio 
@@ -61,10 +59,10 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+        InlineKeyboardButton("❣️ Developer", url = "https://venom_is_alive")
     ],[
-        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_botz')
+        InlineKeyboardButton('🔍 Any issue', url='https://t.me/venom_is_alive'),
+        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ', url='https://t.me/venom_is_alive')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
